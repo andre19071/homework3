2f9a78a4ae75b6364f8279c2cc530c424f77988e
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-16 14:47:27 +0700

    doptask

--- task19/Program.cs
+++ task19/Program.cs
@@ -4,3 +4,13 @@
 //14212 -> нет
 //12821 -> да
 //23432 -> да 
+
+Console.WriteLine("Введите число");
+int string = Convert.ToString(Console.ReadLine());
+
+static bool IsPalindrome(string s)
+{
+    for (int i = 0; i < s.Length / 2; ++i)
+        if (s[i] != s[s.Length - 1 - i]) return false;
+    return true;
+}
